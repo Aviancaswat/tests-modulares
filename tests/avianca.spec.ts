20da773b8052b3f517d4f6426ad5c47bd97b0bc0
fainnerramirez <fainnerramirez@gmail.com>
2025-05-16 15:52:15 -0500

    feat: change

--- tests/avianca.spec.ts
+++ tests/avianca.spec.ts
@@ -2,6 +2,7 @@ import { test } from '../utils/test-extends';
 
 test.describe('página de avianca', () => {
     test('test flujos de avianca', async ({ page }) => {
+        //configuración inicial
         await page.configInitialTest();
         await page.homePageAvianca();
         await page.flightPageAvianca();
