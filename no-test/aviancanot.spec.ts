47fd175c6850b5682246c238d74ef4ca40a4819b
fainnerramirez <fainnerramirez@gmail.com>
2025-05-16 15:12:55 -0500

    feat: update configuration

---
+++ no-test/aviancanot.spec.ts
@@ -0,0 +1,8 @@
+import { test } from '../utils/test-extends';
+
+test.describe('página de avianca', () => {
+    test('test flujos de avianca', async ({ page }) => {
+        await page.configInitialTest();
+        await page.homePageAvianca();
+    });
+});
